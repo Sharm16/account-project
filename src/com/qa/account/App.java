b98dd2fbe4d5bcc64ec171efe602069b65e3d15b
Sharm16 <shyam-s@live.co.uk>
2018-07-10 10:04:02 +0100

    First commit

---
+++ src/com/qa/account/App.java
@@ -0,0 +1,9 @@
+package com.qa.account;
+
+public class App {
+	
+	public String print() {
+		return "Hello world to the standard out";
+	}
+
+}
---
+++ src/com/qa/account/Main.java
@@ -0,0 +1,11 @@
+package com.qa.account;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		App a = new App();
+		System.out.println(a.print());
+	}
+
+}
